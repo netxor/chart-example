5c28b69c43ac43357adca7ee18262a33b5d78192
netxor <netxor0b@gmail.com>
2016-02-10 14:10:09 +0600

    fixed the main file main.js

--- js/main.js
+++ js/main.js
@@ -20,37 +20,77 @@ function ChangeChartType(chart, series, newType) {
     }
 }
 */
+var data = [ {
+  "id" : 4,
+  "index" : 1.76,
+  "river" : "р.Буктырма (ВКО)"
+}, {
+  "id" : 9,
+  "index" : 12.46,
+  "river" : "р.Красноярка (ВКО)"
+}, {
+  "id" : 11,
+  "index" : 1.64,
+  "river" : "р. Емель (ВКО)"
+}, {
+  "id" : 16,
+  "index" : 0,
+  "river" : "р. Жайык (Атырауская)"
+}, {
+  "id" : 23,
+  "index" : 2.3,
+  "river" : "р. Елек (ЗКО)"
+}, {
+  "id" : 28,
+  "index" : 1.29,
+  "river" : "р. Каргалы (Актюбинская)"
+}, {
+  "id" : 30,
+  "index" : 1.39,
+  "river" : "р. Ыргыз (Актюбинская)"
+}, {
+  "id" : 35,
+  "index" : 5.07,
+  "river" : "р.Тобыл (Костанайская)"
+}, {
+  "id" : 42,
+  "index" : 2.48,
+  "river" : "р.Беттыбулак (Акмолинская)"
+}, {
+  "id" : 47,
+  "index" : 5.28,
+  "river" : "р.Нура (Карагандинская)"
+} ];
 
-// Two charts definition
-var chart1, chart2;
+$.each(data, function (i, point) {
+    point.y = point.index;
+    point.name = point.river;
+});
 
-// Once DOM (document) is finished loading
-$(document).ready(function() {
-
-    var url = 'http://data.egov.kz/api/v2/pollut_river/data?pretty';
-
-    $.getJSON(url, function(data) {
-
-      
     // First chart initialization
-    chart1 = new Highcharts.Chart({
-     chart: {
+var chart1 = new Highcharts.Chart({
+
+    chart: {
         renderTo: 'chart_1',
         type: 'column',
         height: 350,
-     },
-     title: {
+    },
+
+    title: {
         text: 'Изменения комплексного индекса загрязненности воды на реках Республики Казахстан'
-     },
-     xAxis: {
+    },
+
+    xAxis: {
         categories: ['р. Буктырма (ВКО)', 'р. Красноярка (ВКО)', 'р. Емель (ВКО)', 'р. Жайык (Атырауская)', 'р. Елек (ЗКО)', 'р. Каргалы (Актюбинская)', 'р. Ыргыз (Актюбинская)', 'р. Тобыл (Костанайская)', 'р. Беттыбулак (Акмолинская)', 'р. Нура (Карагандинская)']
-     },
-     yAxis: {
+    },
+
+    yAxis: {
         title: {
-           text: 'Процент Загрязнения Реки'
+            text: 'Процент Загрязнения Реки'
         }
-     },
-     plotOptions: {
+    },
+
+    plotOptions: {
         series: {
             borderWidth: 0,
             dataLabels: {
@@ -58,68 +98,18 @@ $(document).ready(function() {
                 format: '{point.y:.1f}%'
             }
         }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
      },
-     tooltip: {
-            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-     },
-     series: [{
-       colorByPoint: true,
-       /*
-        name: 'Dev #1',
-        data: [5, 10, 20, 22, 25, 28, 30, 40, 80, 90]
-     }, {
-        name: 'Dev #2',
-        data: [15, 15, 18, 40, 30, 25, 60, 60, 80, 70]
-     }, {
-        name: 'Dev #3',
-        data: [1, 3, 6, 0, 50, 25, 50, 60, 30, 100]
-     }]
-     */
-        data: [{
-            //"id" : 4,
-            y : 1.76,
-            name : "р. Буктырма (ВКО)"
-          }, {
-            //"id" : 9,
-            y : 12.46,
-            name : "р. Красноярка (ВКО)"
-          }, {
-            //"id" : 11,
-            y : 1.64,
-            name : "р. Емель (ВКО)"
-          }, {
-            //"id" : 16,
-            y : 0,
-            name : "р. Жайык (Атырауская)"
-          }, {
-            //"id" : 23,
-            y : 2.3,
-            name : "р. Елек (ЗКО)"
-          }, {
-            //"id" : 28,
-            y : 1.29,
-            name : "р. Каргалы (Актюбинская)"
-          }, {
-            //"id" : 30,
-            y : 1.39,
-            name : "р. Ыргыз (Актюбинская)"
-          }, {
-            //"id" : 35,
-            y : 5.07,
-            name : "р. Тобыл (Костанайская)"
-          }, {
-            //"id" : 42,
-            y : 2.48,
-            name : "р. Беттыбулак (Акмолинская)"
-          }, {
-            //"id" : 47,
-            y : 5.28,
-            name : "р. Нура (Карагандинская)"
-          }]
-        }]
-      });
-    });
+
+    series: [{
+        colorByPoint: true,
+        data: data
+    }]
+});
 // --------------------------------------------------------------------------------
     // Switchers (of the Chart1 type) - onclick handler
     /*
@@ -128,4 +118,3 @@ $(document).ready(function() {
         ChangeChartType(chart1, chart1.series, newType);
     });
     */
-});
